81a43c58bc627d834f51e7800c1281e9ac6fc656
안유진 <Eugenius1st@gmail.com>
2022-05-31 22:46:42 +0900

    [#1]: Server Setup + [#0]

--- src/server.js
+++ src/server.js
@@ -2,11 +2,4 @@ import express from "express";
 
 const app = express();
 console.log("hi");
-// app.set("view engine ", "pug");
-// app.set("views", __dirname + "/views");
-
-// app.get("/", (req, res) => res.render("home"));
-
-// const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen);
 app.listen(3000);
