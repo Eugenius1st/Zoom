0bf6bcefa7c1542d49065dbf974ecedaebea2e14
안유진 <Eugenius1st@gmail.com>
2022-06-01 22:35:04 +0900

    [#2]: Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 import express from "express";
-
+import path from "path";
+const __dirname = path.resolve();
 const app = express();
-console.log("hi");
-app.listen(3000);
+
+// Espress 로 할 일은 views를 설정해주고
+//render 하고
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
